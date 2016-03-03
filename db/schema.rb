881fe4bc9c46d8460a5936900d571da6e53e02fe
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 12:18:56 +1300

    Add type field to Event model for STI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160303231529) do
+ActiveRecord::Schema.define(version: 20160303231803) do
 
   create_table "events", force: :cascade do |t|
     t.string   "state_attribute"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160303231529) do
     t.integer  "game_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "type"
   end
 
   add_index "events", ["game_id"], name: "index_events_on_game_id"
