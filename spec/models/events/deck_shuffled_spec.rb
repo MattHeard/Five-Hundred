75b7ee2086c9ae9a53604072b3b310c077cec33b
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 16:20:03 +1300

    Add tests for factory-built event

--- spec/models/events/deck_shuffled_spec.rb
+++ spec/models/events/deck_shuffled_spec.rb
@@ -20,4 +20,28 @@ RSpec.describe DeckShuffled, type: :model do
       expect(event.value).to be_empty
     end
   end
+
+  context "built from the factory" do
+    subject(:event) { DeckShuffled.build }
+
+    it "should have no associated game" do
+      expect(event.game).to be nil
+    end
+
+    it "should modify the deck" do
+      expect(event.state_attribute).to eq "deck="
+    end
+
+    it "should have an encoded value" do
+      expect(event.encoded_value).to be_present
+    end
+
+    it "should have an array which is not empty for its value" do
+      expect(event.value).not_to be_empty
+    end
+
+    it "should have not have an unshuffled deck for its value" do
+      expect(event.value.join).not_to eq Game::UNSHUFFLED_DECK.join
+    end
+  end
 end
