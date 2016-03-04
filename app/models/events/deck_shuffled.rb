03a39061513c490effaff4024d961923577255f6
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 14:41:44 +1300

    Refactor all references to UNSHUFFLED_DECK

--- app/models/events/deck_shuffled.rb
+++ app/models/events/deck_shuffled.rb
@@ -1,12 +1,4 @@
 class DeckShuffled < Event
-  UNSHUFFLED_DECK = %w{
-    A♠ K♠ Q♠ J♠ 10♠ 9♠ 8♠ 7♠ 6♠ 5♠
-    A♣ K♣ Q♣ J♣ 10♣ 9♣ 8♣ 7♣ 6♣ 5♣
-    A♥ K♥ Q♥ J♥ 10♥ 9♥ 8♥ 7♥ 6♥ 5♥ 4♥
-    A♦ K♦ Q♦ J♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦
-    JOKER
-  }
-
   def self.build
     DeckShuffled.new(state_attribute: :deck=, encoded_value: encode(shuffled_deck))
   end
@@ -18,7 +10,7 @@ class DeckShuffled < Event
   private
 
   def self.shuffled_deck
-    UNSHUFFLED_DECK.shuffle
+    Game::UNSHUFFLED_DECK.shuffle
   end
 
   def self.encode(deck)
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,18 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  UNSHUFFLED_DECK = %w{
-    A♠ K♠ Q♠ J♠ 10♠ 9♠ 8♠ 7♠ 6♠ 5♠
-    A♣ K♣ Q♣ J♣ 10♣ 9♣ 8♣ 7♣ 6♣ 5♣
-    A♥ K♥ Q♥ J♥ 10♥ 9♥ 8♥ 7♥ 6♥ 5♥ 4♥
-    A♦ K♦ Q♦ J♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦
-    JOKER }
-
   describe "#deck" do
     subject(:game) { Game.new }
 
     specify "a new game has an unshuffled deck" do
-      expect(game.deck.join).to eq UNSHUFFLED_DECK.join
+      expect(game.deck.join).to eq Game::UNSHUFFLED_DECK.join
     end
   end
 end
