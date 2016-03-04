69b8f1939e2da7b13c5ee5fec295d7d0141e08e1
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 13:38:16 +1300

    Define DeckShuffled event and add to Game

--- app/models/events/deck_shuffled.rb
+++ app/models/events/deck_shuffled.rb
@@ -1,2 +1,27 @@
 class DeckShuffled < Event
+  UNSHUFFLED_DECK = %w{
+    A♠ K♠ Q♠ J♠ 10♠ 9♠ 8♠ 7♠ 6♠ 5♠
+    A♣ K♣ Q♣ J♣ 10♣ 9♣ 8♣ 7♣ 6♣ 5♣
+    A♥ K♥ Q♥ J♥ 10♥ 9♥ 8♥ 7♥ 6♥ 5♥ 4♥
+    A♦ K♦ Q♦ J♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦
+    JOKER
+  }
+
+  def self.build
+    DeckShuffled.new(state_attribute: :deck=, encoded_value: encode(shuffled_deck))
+  end
+
+  def value
+    encoded_value.split
+  end
+
+  private
+
+  def self.shuffled_deck
+    UNSHUFFLED_DECK.shuffle
+  end
+
+  def self.encode(deck)
+    deck.map { |card| (card << (" " * 6))[0, 6] }.join
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,21 @@
 class Game < ActiveRecord::Base
+  has_many :events, dependent: :destroy
+
   UNSHUFFLED_DECK = %w{
     A♠ K♠ Q♠ J♠ 10♠ 9♠ 8♠ 7♠ 6♠ 5♠
     A♣ K♣ Q♣ J♣ 10♣ 9♣ 8♣ 7♣ 6♣ 5♣
     A♥ K♥ Q♥ J♥ 10♥ 9♥ 8♥ 7♥ 6♥ 5♥ 4♥
     A♦ K♦ Q♦ J♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦
-    JOKER }
+    JOKER
+  }
+
+  attr_writer :deck
 
   def deck
     @deck || UNSHUFFLED_DECK
+  end
+
+  def update
+    events.each { |event| send(event.state_attribute, event.value) }
   end
 end
