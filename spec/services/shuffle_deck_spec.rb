101969e233c5e1d3752c0edfab57ff00dc5758f4
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 15:04:52 +1300

    Expect ShuffleDeck service to return true on success

--- spec/services/shuffle_deck_spec.rb
+++ spec/services/shuffle_deck_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ShuffleDeck do
     specify "it should shuffle the deck" do
       expect(game.deck.join).to eq Game::UNSHUFFLED_DECK.join
       expect(game.events).to be_empty
-      ShuffleDeck.new(game).call
+      expect(ShuffleDeck.new(game).call).to be true
       expect(game.deck.join).not_to eq Game::UNSHUFFLED_DECK.join
       expect(game.events).to have_exactly(1).item
     end
