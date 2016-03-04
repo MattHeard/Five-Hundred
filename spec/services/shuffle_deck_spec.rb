84e6a03f5c9623a78dedb1b1abec56ca1a30c840
Matt Heard <matt.heard@powershop.co.nz>
2016-03-04 15:02:56 +1300

    Test ShuffleDeck service

--- spec/services/shuffle_deck_spec.rb
+++ spec/services/shuffle_deck_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ShuffleDeck do
+  context "with a new game" do
+    subject(:game) { Game.create }
+
+    specify "it should shuffle the deck" do
+      expect(game.deck.join).to eq Game::UNSHUFFLED_DECK.join
+      expect(game.events).to be_empty
+      ShuffleDeck.new(game).call
+      expect(game.deck.join).not_to eq Game::UNSHUFFLED_DECK.join
+      expect(game.events).to have_exactly(1).item
+    end
+  end
 end
