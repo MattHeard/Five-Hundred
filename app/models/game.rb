4801af50dcb942aaf69ea7e2019457a7f0abec83
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:26:03 +1300

    Add UNSHUFFLED_DECK constant

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ActiveRecord::Base
+  UNSHUFFLED_DECK = %w{
+    A♠ K♠ Q♠ J♠ 10♠ 9♠ 8♠ 7♠ 6♠ 5♠
+    A♣ K♣ Q♣ J♣ 10♣ 9♣ 8♣ 7♣ 6♣ 5♣
+    A♥ K♥ Q♥ J♥ 10♥ 9♥ 8♥ 7♥ 6♥ 5♥ 4♥
+    A♦ K♦ Q♦ J♦ 10♦ 9♦ 8♦ 7♦ 6♦ 5♦ 4♦
+    JOKER
+  }
+
   def deck
     []
   end
