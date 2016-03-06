9b943976b0028228152bbee76c12b24b5d4f043e
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:37:37 +1300

    Change step descriptions to present tense

--- features/shuffle_deck.feature
+++ features/shuffle_deck.feature
@@ -1,5 +1,5 @@
 Feature: Shuffling the deck
   Scenario: The deck is shuffled in a new game
     Given I start a new game
-    Then the deck will have 43 cards
-    And the deck will be shuffled
+    Then the deck has 43 cards
+    And the deck is shuffled
--- features/step_definitions/shuffle_deck_steps.rb
+++ features/step_definitions/shuffle_deck_steps.rb
@@ -2,10 +2,10 @@ Given(/^I start a new game$/) do
   @game = Game.create
 end
 
-Then(/^the deck will have (\d+) cards$/) do |number_of_cards|
+Then(/^the deck has (\d+) cards$/) do |number_of_cards|
   expect(@game.deck.size).to eq number_of_cards.to_i
 end
 
-Then(/^the deck will be shuffled$/) do
+Then(/^the deck is shuffled$/) do
   expect(@game.deck.join).not_to eq Game::UNSHUFFLED_DECK.join
 end
