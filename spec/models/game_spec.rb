5f6cae41d178917b9fa002a6603478ae47d8a6e3
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:14:38 +1300

    Add test for deck method presence

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Game, type: :model do
   describe "#deck" do
     subject(:game) { Game.create }
 
+    it "should respond to #deck" do
+      expect(game).to respond_to(:deck)
+    end
+
     it "not to be nil" do
       expect(game.deck).not_to be nil
     end
