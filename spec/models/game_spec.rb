c53663a4e19a44a2d07e2cd6636a25db341de96a
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:02:03 +1300

    Remove event model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  describe "#deck" do
-    subject(:game) { Game.new }
-
-    specify "a new game has an unshuffled deck" do
-      expect(game.deck.join).to eq Game::UNSHUFFLED_DECK.join
-    end
-  end
 end
