170bfb1af0f3be9ea3a7ee84d85665c2fc196dbc
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:05:57 +1300

    Define pending steps

---
+++ features/step_definitions/shuffle_deck_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I start a new game$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the deck will be shuffled$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
