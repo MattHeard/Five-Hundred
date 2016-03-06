cd808d402539deed6353434dbb3659656f8bc118
Matt Heard <matt.heard@powershop.co.nz>
2016-03-06 16:27:38 +1300

    Add new test condition for Cucumber test

--- features/step_definitions/shuffle_deck_steps.rb
+++ features/step_definitions/shuffle_deck_steps.rb
@@ -2,6 +2,10 @@ Given(/^I start a new game$/) do
   @game = Game.create
 end
 
+Then(/^the deck will have (\d+) cards$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 Then(/^the deck will be shuffled$/) do
   expect(@game.deck.join).not_to eq Game::UNSHUFFLED_DECK.join
 end
