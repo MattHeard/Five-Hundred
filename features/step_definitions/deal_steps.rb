ac8e85a90be8992205e9a9df1f51e8c74b6d392b
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 11:57:35 +1300

    Add step definition for dealing a card to the southern player

--- features/step_definitions/deal_steps.rb
+++ features/step_definitions/deal_steps.rb
@@ -2,3 +2,8 @@ Given(/^a new game has been started$/) do
   game = Game.create
   @id = game.id
 end
+
+When(/^a card is dealt to the southern player$/) do
+  game = Game.find(@id)
+  DealCardToTheSouthernPlayer.new(game).call
+end
