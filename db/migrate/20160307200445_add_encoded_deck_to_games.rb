c644053ca062d6998ee6667f5a7351407f43c594
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 11:36:20 +1300

    Delete game model

--- db/migrate/20160307200445_add_encoded_deck_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddEncodedDeckToGames < ActiveRecord::Migration
-  def change
-    add_column :games, :encoded_deck, :string, null: false, default: ""
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307200445) do
-
-  create_table "games", force: :cascade do |t|
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.string   "encoded_deck", default: "", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
