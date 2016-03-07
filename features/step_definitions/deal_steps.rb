cbeb2554471bf299ebe5511820128fbff10c9aa6
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 12:20:27 +1300

    Create step definition for counting deck

--- features/step_definitions/deal_steps.rb
+++ features/step_definitions/deal_steps.rb
@@ -7,3 +7,9 @@ When(/^a card is dealt to the southern player$/) do
   game = Game.find(@id)
   DealCardToTheSouthernPlayer.new(game).call
 end
+
+Then(/^the deck has (\d+) cards$/) do |number_of_cards|
+  game = Game.find(@id)
+  deck = Deck.new(game).call
+  expect(deck).to have_exactly(number_of_cards.to_i).items
+end
