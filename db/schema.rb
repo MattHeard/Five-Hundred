d2a4b4398e47dc901086fbdea48356ce8f9f1efa
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 12:00:18 +1300

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307224329) do
+ActiveRecord::Schema.define(version: 20160307225804) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
