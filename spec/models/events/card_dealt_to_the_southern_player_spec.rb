5c14baf38f756fd558eeb398de4bd9c771d9359e
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 15:47:16 +1300

    Pass a card to the CardDealtToTheSouthernPlayer event

--- spec/models/events/card_dealt_to_the_southern_player_spec.rb
+++ spec/models/events/card_dealt_to_the_southern_player_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CardDealtToTheSouthernPlayer, type: :model do
+  let(:card) { "JOKER" }
+
+  subject(:event) { CardDealtToTheSouthernPlayer.new(card) }
+
+  it "is present" do
+    expect(:event).to be_present
+  end
 end
