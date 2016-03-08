4aacf1acb3ec1c8cd4f645d69cc3d451ebbe162d
Matt Heard <matt.heard@powershop.co.nz>
2016-03-08 15:48:49 +1300

    Revert "Add association between a game and its events"
    This reverts commit c80b283763727ba3a9c24f04b35c7ee1c7bc1d3b.
    
    This commit had broken rspec tests.

--- db/migrate/20160308021337_add_game_ref_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddGameRefToEvents < ActiveRecord::Migration
-  def change
-    add_reference :events, :game, index: true, foreign_key: true
-  end
-end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  subject(:game) { Game.create }
-
-  context "with no modifications" do
-    it "has no events" do
-      expect(game.events).to be_empty
-    end
-  end
 end
