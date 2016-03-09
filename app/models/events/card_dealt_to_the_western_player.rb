8f288196604b32c2b0621aafe33ad106dcf0465c
Matt Heard <matt.heard@powershop.co.nz>
2016-03-09 19:39:32 +1300

    Extend CardDealt instead of Event

--- app/models/events/card_dealt_to_the_western_player.rb
+++ app/models/events/card_dealt_to_the_western_player.rb
@@ -1,4 +1,4 @@
-class CardDealtToTheWesternPlayer < Event
+class CardDealtToTheWesternPlayer < CardDealt
   def apply(game_state)
     game_state.deck.delete(card)
     game_state.hands[:west] << card
