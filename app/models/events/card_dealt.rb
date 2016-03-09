46382442d5b83cee4973d6e9fa307c1f654afc60
Matt Heard <matt.heard@powershop.co.nz>
2016-03-10 08:39:33 +1300

    Extract player method for clarity

--- app/models/events/card_dealt.rb
+++ app/models/events/card_dealt.rb
@@ -1,6 +1,12 @@
 class CardDealt < Event
   def apply(game_state)
     game_state.deck.delete(card)
-    game_state.hands[target_player.to_sym] << card
+    game_state.hands[player] << card
+  end
+
+  private
+
+  def player
+    target_player.to_sym
   end
 end
