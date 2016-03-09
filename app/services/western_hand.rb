d9c6e65ce82a88a641f05265c43ebd01f058fc9a
Matt Heard <matt.heard@powershop.co.nz>
2016-03-09 15:04:08 +1300

    Use GameState factory again

--- app/services/western_hand.rb
+++ app/services/western_hand.rb
@@ -4,11 +4,6 @@ class WesternHand
   end
 
   def call
-    game_state = @game.events.inject(GameState.new) do |state, event|
-      event.apply(state)
-      state
-    end
-
-    game_state.western_hand
+    GameState.for(@game).western_hand
   end
 end
