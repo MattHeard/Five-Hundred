9ad8062c8495b99a91c62b9522826cb0f8ca49f2
Matt Heard <matt.heard@powershop.co.nz>
2016-03-10 09:44:23 +1300

    Rewrite GameState spec for clarity

--- spec/concepts/game_state_spec.rb
+++ spec/concepts/game_state_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe GameState do
   subject(:game_state) { GameState.new }
 
-  describe "#deck" do
-    it "should have a size" do
-      expect(game_state.deck).to respond_to(:size)
+  context "with no modifications" do
+    describe "#deck" do
+      it "should have 43 cards" do
+        expect(game_state.deck).to have_exactly(43).items
+      end
     end
   end
 end
