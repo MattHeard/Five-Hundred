61f9f10cb8b210b233b6067ceaa1fdc47edb2fce
Matt Heard <matt.heard@powershop.co.nz>
2016-03-10 10:02:40 +1300

    Add cards to the kitty with CardDealt

--- app/models/events/card_dealt.rb
+++ app/models/events/card_dealt.rb
@@ -1,7 +1,11 @@
 class CardDealt < Event
   def apply(game_state)
     game_state.deck.delete(card)
-    game_state.hands[player] << card
+    if player == :kitty
+      game_state.kitty << card
+    else
+      game_state.hands[player] << card
+    end
   end
 
   private
