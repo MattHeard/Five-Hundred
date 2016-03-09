6f0a5197639cdd9eab51e7ce0d82df32a0e58b85
Matt Heard <matt.heard@powershop.co.nz>
2016-03-10 12:45:08 +1300

    Define step for verifying that there is a dealer

--- features/step_definitions/dealer_steps.rb
+++ features/step_definitions/dealer_steps.rb
@@ -8,3 +8,9 @@ When(/^the dealer is changed$/) do
   game = Game.find(@id)
   ChangeDealer.new(game).call
 end
+
+Then(/^there will be a dealer$/) do
+  game = Game.find(@id)
+  dealer = GameState.for(game).dealer
+  expect(dealer).not_to be nil
+end
