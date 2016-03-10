c3540a68a50f4be426bc3c6fe3b75edfcab34e96
Matt Heard <matt.heard@powershop.co.nz>
2016-03-10 15:25:38 +1300

    Update controllers and views to display game state

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,20 @@
+require 'pp'
+
 class GamesController < ApplicationController
   def index
   end
 
   def create
     game = Game.create!
+    DealAllCards.new(game).call
+    ChangeDealer.new(game).call
 
     redirect_to game
   end
 
   def show
-    @game = Game.find(params[:id])
-
-    @game.deal
+    game = Game.find(params[:id])
+    @game_state = GameState.for(game)
+    pp @game_state
   end
 end
