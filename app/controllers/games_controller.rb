a190135a98f1908b80c4eea05fb25240d57d913d
Matt Heard <matt.heard@powershop.co.nz>
2016-03-11 13:21:37 +1300

    Add view elements for making a bid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,7 +18,11 @@ class GamesController < ApplicationController
   def update
     game = Game.find(update_params[:id])
 
-    PassBid.new(game).call if update_params[:bid_or_pass] == "Pass"
+    if bidder_has_passed?
+      PassBid.new(game).call
+    elsif bidder_has_made_bid?
+      MakeBid.new(game, number_of_tricks, trump_suit).call
+    end
 
     redirect_to game
   end
@@ -26,6 +30,22 @@ class GamesController < ApplicationController
   private
 
   def update_params
-    params.permit(:id, :bid_or_pass)
+    params.permit(:id, :bid_or_pass, :number_of_tricks, :trump_suit)
+  end
+
+  def bidder_has_passed?
+    update_params[:bid_or_pass] == "Pass"
+  end
+
+  def bidder_has_made_bid?
+    update_params[:bid_or_pass] == "Bid"
+  end
+
+  def number_of_tricks
+    update_params[:number_of_tricks]
+  end
+
+  def trump_suit
+    update_params[:trump_suit]
   end
 end
