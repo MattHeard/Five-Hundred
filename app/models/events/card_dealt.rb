780489828ad513c273726087fc2ccaa05c45aa54
Matt Heard <matt.heard@powershop.co.nz>
2016-03-16 12:35:07 +1300

    Refactor ternary if into a method

--- app/models/events/card_dealt.rb
+++ app/models/events/card_dealt.rb
@@ -1,14 +1,14 @@
 class CardDealt < Event
   def apply(game_state)
     game_state.deck.delete(card)
-    if player == :kitty
-      game_state.kitty << card
-    else
-      game_state.hands[player] << card
-    end
+    destination(game_state) << card
   end
 
   private
+
+  def destination(game_state)
+    player == :kitty ? game_state.kitty : game_state.hands[player]
+  end
 
   def player
     target_player.to_sym
