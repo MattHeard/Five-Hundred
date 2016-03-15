b4c8b2e0bab3438dd1310f2c9b96a6d1e94b0ac8
Matt Heard <matt.heard@powershop.co.nz>
2016-03-16 12:31:15 +1300

    Refactor passing bid into a constant hash

--- app/models/events/bid_passed.rb
+++ app/models/events/bid_passed.rb
@@ -1,17 +1,8 @@
 class BidPassed < Event
+  PASSING_BID = { bid_or_pass: :pass }
   def apply(game_state)
     current_bidder = game_state.bidder
-    game_state.bids[current_bidder] = bid
+    game_state.bids[current_bidder] = PASSING_BID
     game_state.bidder = NextBidder.new(game_state).call
-  end
-
-  private
-
-  def bid
-    {
-      bid_or_pass: :pass,
-      number_of_tricks: nil,
-      trump_suit: nil
-    }
   end
 end
