5722f31a77388166143b4ce37ae2891ec97b2f83
Matt Heard <matt.heard@powershop.co.nz>
2016-03-22 09:54:44 +1300

    Refactor to extract let variables and subject

--- spec/services/redeal_all_cards_spec.rb
+++ spec/services/redeal_all_cards_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe RedealAllCards do
   describe "#initialize" do
+    subject(:service) { RedealAllCards.new(arg) }
+
+    let(:arg) { 0 }
+
     it "should be present" do
-      arg = 0
-      expect(RedealAllCards.new(arg)).to be_present
+      expect(service).to be_present
     end
   end
 end
