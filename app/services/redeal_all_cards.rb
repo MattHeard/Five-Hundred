50d673e00b5433a8b95d34c3d14121f33409fae1
Matt Heard <matt.heard@powershop.co.nz>
2016-03-22 09:56:01 +1300

    Add #call

--- app/services/redeal_all_cards.rb
+++ app/services/redeal_all_cards.rb
@@ -1,4 +1,7 @@
 class RedealAllCards
   def initialize(arg)
   end
+
+  def call
+  end
 end
--- spec/services/redeal_all_cards_spec.rb
+++ spec/services/redeal_all_cards_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RedealAllCards do
+  subject(:service) { RedealAllCards.new(arg) }
+
+  let(:arg) { 0 }
+
   describe "#initialize" do
-    subject(:service) { RedealAllCards.new(arg) }
-
-    let(:arg) { 0 }
-
     it "should be present" do
       expect(service).to be_present
+    end
+  end
+
+  describe "#call" do
+    it "should be nil" do
+      expect(service.call).to be nil
     end
   end
 end
