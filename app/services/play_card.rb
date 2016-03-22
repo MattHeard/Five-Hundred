3c6679d7a0bb091e1c8a419afd872107322466e7
Matt Heard <matt.heard@powershop.co.nz>
2016-03-23 12:13:54 +1300

    Modify #initialize to take three args

--- app/services/play_card.rb
+++ app/services/play_card.rb
@@ -1,2 +1,4 @@
 class PlayCard
+  def initialize(arg0, arg1, arg2)
+  end
 end
--- spec/services/play_card_spec.rb
+++ spec/services/play_card_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PlayCard do
+  describe "#initialize" do
+    it "takes three args" do
+      arg0 = arg1 = arg2 = nil
+      expect { PlayCard.new(arg0, arg1, arg2) }.not_to raise_error
+    end
+  end
 end
