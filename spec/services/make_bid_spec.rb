d282d1cd22d47771f6263fe4c675e1dea2edc0c6
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 10:29:47 +1300

    Reorganise MakeBid RSpec test

--- spec/services/make_bid_spec.rb
+++ spec/services/make_bid_spec.rb
@@ -3,49 +3,32 @@ require 'rails_helper'
 RSpec.describe MakeBid do
   let(:number_of_tricks) { 6 }
   let(:trump_suit) { "♠" }
-  let(:game) do
-    Game.create!
-  end
+  let(:game) { Game.create! }
+  subject(:service) { MakeBid.new(game, number_of_tricks, trump_suit) }
 
-  context "with no previous bids" do
-    before do
-      ChangeDealer.new(game).call
-      MakeBid.new(game, number_of_tricks, trump_suit).call
-      game.reload
-    end
+  before { ChangeDealer.new(game).call }
 
-    describe "#call" do
+  describe "#call" do
+    context "with no previous bids" do
       it "adds a BidMade event to the game" do
+        service.call
+        game.reload
         expect(game.events.second).to be_instance_of BidMade
       end
     end
-  end
 
-  context "with a previous bid of 6 Spades" do
-    before do
-      ChangeDealer.new(game).call
-      MakeBid.new(game, number_of_tricks, trump_suit).call
-      game.reload
-    end
-
-    describe "#call" do
-      it "returns false when a second bid of 6 Spades is made" do
-        service = MakeBid.new(game, number_of_tricks, trump_suit)
+    context "with a previous bid of 6 Spades" do
+      it "fails when a second bid of 6 Spades is made" do
+        service.call
+        game.reload
         expect(service.call).to be false
       end
     end
-  end
 
-  context "after the bidder has previously passed" do
-    before do
-      ChangeDealer.new(game).call
-      4.times { PassBid.new(game).call }
-      game.reload
-    end
-
-    describe "#call" do
-      it "returns false when a second bid of 6 Spades is made" do
-        service = MakeBid.new(game, number_of_tricks, trump_suit)
+    context "after the bidder has previously passed" do
+      it "fails when a second bid of 6 Spades is made" do
+        4.times { PassBid.new(game).call }
+        game.reload
         expect(service.call).to be false
       end
     end
