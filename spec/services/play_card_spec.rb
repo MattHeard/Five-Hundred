58ba6739d802a40b0de4280248751a49e869750c
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 11:45:53 +1300

    Reorganise PlayCard RSpec test

--- spec/services/play_card_spec.rb
+++ spec/services/play_card_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PlayCard do
-  subject(:service) { PlayCard.new(game, player, card) }
-
-  let(:game) { Game.create! }
-  let(:player) { :south }
-  let(:card) { GameState.for(game).hands[player].first }
-
   describe "#call" do
-    before do
-      DealAllCards.new(game).call
-      service.call
-    end
+    subject(:service) { PlayCard.new(game, player, card) }
+
+    let(:game) { Game.create! }
+    let(:player) { :south }
+    let(:card) { GameState.for(game).hands[player].first }
 
     it "adds a CardPlayed event to the game" do
+      DealAllCards.new(game).call
+      service.call
       expect(game.events.last).to be_instance_of CardPlayed
     end
   end
