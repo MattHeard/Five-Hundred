990c9dd1a9ac214514a548303274e5ee295ed1a3
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 10:13:57 +1300

    Reorganise CollectCards RSpec test

--- spec/services/collect_cards_spec.rb
+++ spec/services/collect_cards_spec.rb
@@ -1,21 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CollectCards do
-  subject(:service) { CollectCards.new(game) }
-
-  let(:game) { Game.create! }
-
-  describe "#initialize" do
-    it "is present" do
-      expect(service).to be_present
-    end
-  end
-
   describe "#call" do
-    before { service.call }
+    subject(:service) { CollectCards.new(game) }
+
+    let(:game) { Game.create! }
 
     it "adds a CardsCollected event to the game" do
-      expect(game.events.first).to be_instance_of CardsCollected
+      service.call
+      expect(game.events.last).to be_instance_of CardsCollected
     end
   end
 end
