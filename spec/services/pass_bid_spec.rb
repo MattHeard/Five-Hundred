68338774c2c34b9b5f6014b70afd1add1d005bb0
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 11:06:47 +1300

    Reorganise BidPassed RSpec test

--- spec/services/pass_bid_spec.rb
+++ spec/services/pass_bid_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PassBid do
-  let(:game) do
-    game = Game.create!
-    PassBid.new(game).call
-    game.reload
-    game
-  end
-
   describe "#call" do
+    subject(:service) { PassBid.new(game) }
+
+    let(:game) { Game.create! }
+
     it "adds a BidPassed event to the game" do
-      expect(game.events.first).to be_instance_of BidPassed
+      service.call
+      game.reload
+      expect(game.events.last).to be_instance_of BidPassed
     end
   end
 end
