808be8fadee68eb6d70594c8c1507f143dbce88d
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 12:24:44 +1300

    Inline unnecessary 'before' block

--- spec/services/redeal_all_cards_spec.rb
+++ spec/services/redeal_all_cards_spec.rb
@@ -1,20 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RedealAllCards do
-  subject(:service) { RedealAllCards.new(game) }
-
-  let(:game) { Game.create! }
-
-  describe "#initialize" do
-    it "should be present" do
-      expect(service).to be_present
-    end
-  end
-
   describe "#call" do
-    before { service.call }
+    subject(:service) { RedealAllCards.new(game) }
+
+    let(:game) { Game.create! }
 
     it "adds a CardsCollected event to the game" do
+      service.call
       expect(game.events.first).to be_instance_of CardsCollected
     end
   end
