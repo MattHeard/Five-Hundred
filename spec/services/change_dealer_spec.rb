5e2850ce39e086d8d7a38ae861f015a12fb83665
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 12:23:21 +1300

    Add test case for cycling the dealer

--- spec/services/change_dealer_spec.rb
+++ spec/services/change_dealer_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe ChangeDealer do
 
     let(:game) { Game.create! }
 
-    it "adds a DealerChanged event to the game" do
-      service.call
-      game.reload
-      expect(game.events.last).to be_instance_of DealerChanged
+    context "with no previous dealer" do
+      it "adds a DealerChanged event to the game" do
+        service.call
+        game.reload
+        expect(game.events.last).to be_instance_of DealerChanged
+      end
+    end
+
+    context "with a previous dealer" do
+      it "adds a DealerChanged event to the game" do
+        2.times { service.call }
+        game.reload
+        expect(game.events.last).to be_instance_of DealerChanged
+      end
     end
   end
 end
