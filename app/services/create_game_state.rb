b371f9b791d1127255c1a9ae9c56adb95d978469
Matt Heard <matt.heard@powershop.co.nz>
2016-03-24 15:13:45 +1300

    Add #call

--- app/services/create_game_state.rb
+++ app/services/create_game_state.rb
@@ -1,2 +1,8 @@
 class CreateGameState
+  def initialize(game)
+  end
+
+  def call
+    0
+  end
 end
--- spec/services/create_game_state_spec.rb
+++ spec/services/create_game_state_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CreateGameState do
+  describe "#call" do
+    it "returns a GameState" do
+      game = Game.create!
+      expect(CreateGameState.new(game).call).to be_present
+    end
+  end
 end
