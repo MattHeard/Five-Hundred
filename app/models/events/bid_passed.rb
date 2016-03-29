8d885ccbacee5b27723502b496eba2606ec2e4c5
Matt Heard <matt.heard@powershop.co.nz>
2016-03-29 15:51:05 +1300

    Reuse NextPlayer service

--- app/models/events/bid_passed.rb
+++ app/models/events/bid_passed.rb
@@ -3,7 +3,7 @@ class BidPassed < Event
   def apply(game_state)
     current_bidder = game_state.bidder
     game_state.bids[current_bidder] = PASSING_BID
-    game_state.bidder = NextBidder.new(game_state).call
+    game_state.bidder = NextPlayer.new(current_bidder).call
 
     game_state
   end
