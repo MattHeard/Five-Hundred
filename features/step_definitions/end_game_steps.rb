77fb8e8ebf21906fc0e14d1c4c01bb26381007cf
Matt Heard <matt.heard@powershop.co.nz>
2016-04-07 09:59:14 +1200

    Add pending steps for ending games

---
+++ features/step_definitions/end_game_steps.rb
@@ -0,0 +1,7 @@
+Then(/^the game is not over$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the game is over$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/step_definitions/setup_steps.rb
+++ features/step_definitions/setup_steps.rb
@@ -5,3 +5,11 @@ end
 Given(/^the game is in the play phase$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+When(/^no points have been won$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^([^" ]*) reaches (-?\d+) points$/) do |team, points_count|
+  pending # Write code here that turns the phrase above into concrete actions
+end
