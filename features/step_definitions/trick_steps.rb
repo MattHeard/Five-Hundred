5b3175d4460d49b8ac3d1cbc80616e7f3d7092fb
Matt Heard <matt.heard@powershop.co.nz>
2016-04-07 09:33:20 +1200

    Add pending steps for ending tricks

--- features/step_definitions/trick_steps.rb
+++ features/step_definitions/trick_steps.rb
@@ -5,3 +5,11 @@ end
 Given(/^([^" ]*) won the first trick$/) do |player|
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+Then(/^the trick is complete$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the trick is not complete$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
