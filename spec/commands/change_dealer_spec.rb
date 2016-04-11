4aa7976b0e8e4af2f62d69a37f52b394c85643ff
Matt Heard <matt.heard@powershop.co.nz>
2016-04-11 13:42:39 +1200

    Add unimplemented spec

---
+++ spec/commands/change_dealer_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe ChangeDealer do
+  describe "#call" do
+    it "adds one event to the game"
+    it "causes the dealer to change in the game state"
+  end
+end
