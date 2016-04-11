589f2c8662444131e64f62a49bdcd038bd87ab0e
Matt Heard <matt.heard@powershop.co.nz>
2016-04-12 07:49:17 +1200

    Describe messages in the interface to test

--- spec/commands/create_game_state_spec.rb
+++ spec/commands/create_game_state_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+# Messages:
+# 2a.   initialize    - incoming command  - do     unit test
+#   NOTE: initialize will not be guarded, so a unit test will not be meaningful
+# 6a.   call          - incoming query    - do     unit test
+# 7a.   game          - internal          - do not unit test
+# 7b.   game.events   - outgoing query    - do not unit test
+# 7c.   events.inject - outgoing query    - do not unit test
+# 7d.   event.apply   - outgoing query    - do not unit test
+# 12a.  game          - internal          - do not unit test
+# 14a.  new_state     - internal          - do not unit test
+# 15a.  GameState     - outgoing query    - do not unit test
+# 15b.  GameState.new - outgoing query    - do not unit test
 RSpec.describe CreateGameState do
   describe "#call" do
     it "returns a GameState" do
